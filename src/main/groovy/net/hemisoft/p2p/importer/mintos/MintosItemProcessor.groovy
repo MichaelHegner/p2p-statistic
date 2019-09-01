c880b66eae987243680d47bf9f6a86c455fce8a0
Michael Hegner <michaelhegner77@gmail.com>
2019-09-01 08:55:09 +0200

    MINTOS: Integrate Plattform into mapping

--- src/main/groovy/net/hemisoft/p2p/importer/mintos/MintosItemProcessor.groovy
+++ src/main/groovy/net/hemisoft/p2p/importer/mintos/MintosItemProcessor.groovy
@@ -2,15 +2,17 @@ package net.hemisoft.p2p.importer.mintos
 
 import org.springframework.batch.item.ItemProcessor
 
+import net.hemisoft.p2p.importer.domain.Plattform
 import net.hemisoft.p2p.importer.domain.TransactionEntity
 
 public class MintosItemProcessor implements ItemProcessor<MintosTransactionDto, TransactionEntity> {
 
 	@Override
 	public TransactionEntity process(MintosTransactionDto dto) throws Exception {
-		def entity    = new TransactionEntity()
-		entity.id     = dto.ID
+		def entity       = new TransactionEntity()
+		entity.id        = dto.ID
 		entity.amount    = dto.investedAmount
+		entity.plattform = Plattform.MINTOS
 		entity
 	}
 
